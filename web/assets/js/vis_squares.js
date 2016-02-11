a38a3829af208d2a02e549e9309cb488884f97dd
martínez <mtnezb@gmail.com>
2016-02-11 16:07:12 +0100

    visSquares, fix xScale bugs

--- web/assets/js/vis_squares.js
+++ web/assets/js/vis_squares.js
@@ -18,9 +18,7 @@ var VisSquares = Class.extend({
     this.yScaleTotals = d3.scale.ordinal();
     this.yScale = d3.scale.ordinal();
     this.colorScale = d3.scale.ordinal();
-                                                
-    // this.colorScale = d3.scale.ordinal().range(['#F9CE4A', '#F5A71A', '#EF8D19', '#C47413', '#FADE8A','#B6CA0A', '#7A9B02', '#e95b46', '#e4001e', '#ba0006']);
-                                                // ['Siria', 'Afganistán', 'Iraq',   'Rusia',   'Serbia',   'Somalia','Pakistán', 'Nigeria', 'Irán',    'Kosovo']
+    
     // Axis
     this.xAxis = d3.svg.axis();
     this.yAxis = d3.svg.axis();
@@ -35,8 +33,7 @@ var VisSquares = Class.extend({
     this.totalApp = null;
 
     // Legend
-    this.legendOrigin = d3.legend.color();
-    // this.legendSex = d3.legend.color();
+    this.legendSquares = d3.legend.color();
 
     // Objects
     this.tooltip = null;
@@ -53,6 +50,7 @@ var VisSquares = Class.extend({
     this.origins = ['Siria', 'Afganistán', 'Iraq', 'Irán', 'Pakistán', 'Somalia', 'Nigeria', 'Rusia', 'Serbia', 'Kosovo'];
     this.ages = ["menos14", "x14a17", "x18a34", "x35a64", "x65mas", "Desconocido"];
     this.sexs = ["Mujeres", "Hombres"];
+    this.countries = ["Alemania", "Francia", "Suecia", "Reino Unido", "Italia", "Bélgica", "Holanda", "Grecia", "Austria", "Dinamarca", "Polonia", "España", "Finlandia", "Hungría", "Bulgaria", "Malta", "Chipre", "Irlanda", "Rumanía", "Luxemburgo", "República Checa", "Lituania", "Eslovaquia", "Eslovenia", "Portugal", "Croacia", "Letonia", "Estonia"]
     this.radius = 6;
     this.opacity = .9;
     this.opacityLow = .5;
@@ -83,24 +81,9 @@ var VisSquares = Class.extend({
     this.svgSquares = d3.select(this.container).append('svg')
         .attr('width', this.width + this.margin.left + this.margin.right)
         .attr('height', this.height + this.margin.top + this.margin.bottom)
-        .attr('class', 'svg_age')
+        .attr('class', 'svg_' + this.measure)
       .append('g')
         .attr('transform', 'translate(' + 0 + ',' + this.margin.top + ')');
-
-
-    // // Set nice category
-    // this.totalApp = {
-    //   "Siria": 137145,
-    //   "Afganistán":  127460,
-    //   "Iraq":  117000,
-    //   "Rusia": 108590,
-    //   "Serbia": 93160,
-    //   "Somalia": 90375,
-    //   "Pakistán": 90165,
-    //   "Nigeria": 63765,
-    //   "Irán": 62045,
-    //   "Kosovo": 61560
-    // }
 
     this.niceCategory = {
       "menos14": "menos de 14",
@@ -153,13 +136,7 @@ var VisSquares = Class.extend({
           .thenBy('year', -1)
       );
 
-      // Get the unique countries, origins and years.
-      
-      this.countries = d3.nest()
-        .key(function(d) { return d.destiny;})
-        .entries(this.dataChart)
-        .map(function(d) { return d.key; });
-      
+      // Get the unique years.
       this.years = d3.nest()
         .key(function(d) { return d.year;})
         .entries(this.dataChart)
@@ -246,19 +223,19 @@ var VisSquares = Class.extend({
         .attr("class", "legend " + this.measure)
         .attr("transform", "translate(" + (this.width - 20)+ "," + 0 + ")");
    
-      this.legendOrigin
+      this.legendSquares
         .shapeWidth(this.xScale.rangeBand() * 0.7)
         .shapeHeight(this.yScale.rangeBand() * 0.7)
         .shapePadding(5)
         .scale(this.colorScale);
 
       if (this.measure == 'age') {
-        this.legendOrigin
+        this.legendSquares
             .labels(this.colorScale.domain().map(function(d) { return this.niceCategory[d]; }.bind(this)))
       }
 
       d3.select(".legend." + this.measure)
-        .call(this.legendOrigin);
+        .call(this.legendSquares);
 
       this.svgSquares.selectAll('.label')
           .attr('class', function(d) { return 'legend label ' + this._normalize(d); }.bind(this))
@@ -292,8 +269,28 @@ var VisSquares = Class.extend({
     }.bind(this)); // end load data
   }, // end render
 
-  updateRender: function(buttonID) {
+  updateRender: function(buttonID, measure) {
+    this.measure = measure; 
 
+    var svg = d3.select('.svg_' + this.measure);
+
+    // Actualizar colorScale
+    if (this.measure == 'origin') {
+      this.colorScale
+        .domain(this.origins)
+        .range(['#ECD078', '#FBCE34', '#FB9A02', '#C68E26', '#C86700', '#6FAA71', '#3A7343', '#D05253', '#BA1C1B', '#700404']);
+    } else if (this.measure == 'age') {
+
+      this.colorScale
+          .domain(this.ages)
+          .range(['#92E98E', '#5CBF88', '#34937A', '#1D6963', '#14474A', '#686868']);
+    } else {  
+      this.colorScale
+          .domain(this.sexs)
+          .range(['#D55B50', '#3C3F4F']);
+    }
+
+    // Move elements
     if (buttonID == 'total') {
 
       // Update xScale to number of elements instead of countries
@@ -307,7 +304,7 @@ var VisSquares = Class.extend({
 
       // Update squares
       this.years.forEach(function(year) {
-        this.svgSquares.selectAll('.square.x' + year)
+        svg.selectAll('.square.x' + year)
           .sort(function(a,b) { return this.colorScale.domain().indexOf(a.win) - this.colorScale.domain().indexOf(b.win); }.bind(this))
           .transition()
           .duration(this.duration)
@@ -325,136 +322,14 @@ var VisSquares = Class.extend({
         .style('opacity', 1);
 
       // Squares position
-      this.svgSquares.selectAll('.square')
+      svg.selectAll('.square')
           .transition()
           .duration(this.duration)
           .attr('x', function(d) { return this.xScale(d.destiny); }.bind(this))
     }
   }, 
 
-
-  // renderTotals: function(urlData) {
-      
-  //   d3.csv(urlData, function(error, csvData){
-  //     if (error) throw error;
-
-  //     // Map the data
-  //     this.dataChart = csvData;
-
-  //     // Filter the accepted_per != NaN
-  //     this.dataChart = this.dataChart.filter(function(d) { return d.win != "NA"; })
-
-  //     this.dataChart.forEach(function(d) { 
-  //       // d.year = this.parseDate(d.year);
-  //       d.Afganistán = +d.Afganistán;
-  //       d.Iraq = +d.Iraq;
-  //       d.Irán = +d.Irán;
-  //       d.Kosovo = +d.Kosovo;
-  //       d.Nigeria = +d.Nigeria;
-  //       d.Pakistán = +d.Pakistán;
-  //       d.Rusia = +d.Rusia;
-  //       d.Serbia = +d.Serbia;
-  //       d.Siria = +d.Siria;
-  //       d.Somalia = +d.Somalia;
-  //       d.total_country = +d.total_country;
-  //       d.dif = +d.dif;
-  //     }.bind(this));
-
-  //     this.origins = ['Siria', 'Afganistán', 'Iraq', 'Rusia', 'Serbia', 'Somalia', 'Pakistán', 'Nigeria', 'Irán', 'Kosovo']
-
-  //     this.dataTotals = d3.nest()
-  //       .key(function(d) { return d.win;}).sortKeys(function(a,b) { return this.origins.indexOf(b) - this.origins.indexOf(a); }.bind(this))
-  //       .entries(this.dataChart);
-
-  //     var valuesLength =  d3.nest()
-  //       .key(function(d) { return d.win;})
-  //       .rollup(function(values) { return values.length; })
-  //       .entries(this.dataChart);
-
-  //     var max = d3.max(valuesLength, function(d) { return d.values; })
-      
-  //     var totalsDomain = [];
-  //     for (var i = 0; i < max; i++) { 
-  //       totalsDomain.push(i.toString())
-  //     }
-
-  //     // Define the scales
-
-  //     this.xScaleTotals
-  //       .domain(totalsDomain)
-  //       .rangeRoundBands([this.margin.left, (this.width - this.margin.right)], .2)
-
-  //     this.yScaleTotals
-  //       .domain(this.origins)
-  //       .rangeRoundBands([0, this.height], .2);
-
-  //     this.colorScale
-  //       .domain(this.origins);
-
-  //     // The Axis
-  //     this.yAxisTotals
-  //         .scale(this.yScaleTotals)
-  //         .tickFormat(function(d) { return d; }.bind(this))
-  //         .orient("left");
-
-  //     //--> DRAW AXIS
-  //     this.svgSquares.append("g")
-  //         .attr("class", "y axis")
-  //         .attr("transform", "translate(" + (this.margin.left * 2) + ",0)")
-  //         .call(this.yAxisTotals);
-
-  //     //--> DRAW BARS & LABELS
-  //     var originGroup = this.svgSquares.selectAll(".origin_total_bar")
-  //         .data(this.dataTotals)
-  //         .enter().append("g")
-  //         .attr('class', '.origin_total_bar')
-  //         .attr("transform", function(d) {
-  //           return "translate(" + this.margin.left + "," + this.yScaleTotals(d.key) + ")";
-  //     }.bind(this));
-
-
-  //     originGroup.selectAll("rect")
-  //         .data(function(d) { return d.values; })
-  //       .enter().append("rect")
-  //         .attr("width", 10)
-  //         .attr("x", function(d, i) { return this.xScaleTotals(i.toString()); }.bind(this))
-  //         .attr("y", 0)
-  //         .attr("height", this.yScaleTotals.rangeBand())
-  //         .style("fill", function(d) { return this.colorScale(d.win); }.bind(this))
-  //         .style('opacity', 0)
-  //         .transition()
-  //         .delay(function(d, i) { return i * (this.duration/100); }.bind(this))
-  //         .duration(this.duration/100)
-  //         .style('opacity', 1);
-
-  //     this.svgSquares.selectAll(".origin_total_label")
-  //       .data(this.dataTotals)
-  //       .enter().append('text')
-  //           .attr('class', 'label ')
-  //           .attr('x', function(d) { return this.xScaleTotals((d.values.length - 1).toString()) + this.margin.left; }.bind(this))
-  //           .attr('y', function(d) { return this.yScaleTotals(d.key) + (this.yScaleTotals.rangeBand()/1.5); }.bind(this))
-  //           .attr('dx', 15)
-  //           .attr('dy', 2)
-  //           .attr('text-anchor', 'start')
-  //           // .text(function(d) { return d.values.length + ' (Total solucitudes: ' + this.totalApp[d.key] + ')'; }.bind(this))
-  //           .text(function(d) { return d.values.length; })
-  //           .style('fill', this.grey)
-  //           .style('font-size', '1em')
-  //           .style('opacity', 0)
-  //         .transition()
-  //           .delay(50 * (this.duration/100))
-  //           .duration(this.duration/2)
-  //           .style('opacity', 1);
-
-  //   }.bind(this))
-  // },
-
   //PRIVATE
-  // _tickValues:  function (scale) {
-  //   var range = scale.domain()[1] - scale.domain()[0];
-  //   var a = range/4;
-  //   return [scale.domain()[0], scale.domain()[0] + a, scale.domain()[0] + (a * 2), scale.domain()[1] - a, scale.domain()[1]];
-  // },
 
   _mouseoverRender: function () {
     var selected = d3.event.target,
@@ -472,8 +347,6 @@ var VisSquares = Class.extend({
         texts.push(a)
       }
     }
-
-
     
     var textValues = '';
     texts.forEach(function(text) { 
